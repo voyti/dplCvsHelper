286054bedaff43f5cf0d78418295f3d9ea6a546d
Voy Tee <wap300@gmail.com>
2020-09-09 01:59:44 +0200

    Initial complete commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TEST_DATA } from './test_data';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,73 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ctpExcelConvereter';
+
+  rawValue: string = '';
+  dispersion: string = '0.1';
+  grids: any;
+  gridsGenerated: boolean;
+  gridsOptions: any[];
+  gridsGenerationStart: boolean;
+
+  constructor() {
+    console.log(TEST_DATA);
+    this.gridsOptions = [];
+  }
+
+  applyRawValue(event) {
+    this.rawValue = event.target.value;
+  }
+
+  generateGrids() {
+    this.gridsGenerationStart = true;
+
+    const rows = this.rawValue.split('\n');
+    const columnHeaders = rows[0].split(/\t/g);
+
+    const gridsData = _.map(columnHeaders, (header, i) => {
+      const depths = _.times(rows.length - 1, (rowNum) => {
+        return (rowNum + 1) * Number(this.dispersion);
+      });
+      const values = _.map(_.tail(rows),  (row) => {
+        const rowCellValues = row.split(/\t/);
+        return rowCellValues[i];
+      });
+
+      const columnDefs = [{ field: 'glebokosc', width: 70, }, { field: header, width: 70, }];
+
+      const rowDataWithNulls = _.map(depths, (depth, i) => {
+        const rowDataEl = {
+          glebokosc: Number(depth).toFixed(1),
+        };
+        rowDataEl[header] = _.isNil(values[i]) || _.isEmpty(values[i])? null : Number(values[i]);
+        return rowDataEl;
+      });
+
+      const rowData = _.reject(rowDataWithNulls, (d) => _.isNil(d[header]));
+      return { rowData, columnDefs };
+    });
+
+    _.forEach(gridsData, (gridData) => {
+      this.gridsOptions[gridData.columnDefs[1].field] = {};
+    });
+
+    setTimeout(() => {
+      this.grids = gridsData;
+      console.log(this.grids);
+
+      setTimeout(() => {
+        this.gridsGenerated = true;
+        this.gridsGenerationStart = false;
+        console.warn(this.gridsOptions);
+      });
+
+    }, 100);
+  }
+
+  downloadAllCVS() {
+    _.forEach(this.grids, (grid) => {
+      const options = this.gridsOptions[grid.columnDefs[1].field];
+      options.api.exportDataAsCsv({ fileName: options.columnDefs[1].field + '.csv' });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgGridModule.withComponents([]),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
